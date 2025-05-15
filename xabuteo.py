d074caf2e6b9f202fbcfbc6e8407020f01e29343
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-16 09:46:38 +1000

    Create xabuteo.py

---
+++ xabuteo.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Xabuteo", layout="wide")
+
+st.title("🏠 Welcome to Xabuteo")
+st.markdown("""Welcome to the world's premier table football online application!""")
