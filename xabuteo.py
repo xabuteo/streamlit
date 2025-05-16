ee25d40edf9c831070e3e23352acc81b02e2a76a
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-16 13:35:41 +1000

    Update xabuteo.py

--- xabuteo.py
+++ xabuteo.py
@@ -1,6 +1,26 @@
 import streamlit as st
+from pages import home, register, login, profile
 
 st.set_page_config(page_title="Xabuteo", layout="wide")
 
-st.title("🏠 Welcome to Xabuteo")
-st.markdown("""Welcome to the world's premier table football online application!""")
+if "user_email" not in st.session_state:
+    st.session_state["user_email"] = None
+
+st.sidebar.title("Xabuteo")
+
+if st.session_state["user_email"]:
+    selection = st.sidebar.radio("Navigation", ["Home", "My Profile", "Logout"])
+else:
+    selection = st.sidebar.radio("Navigation", ["Home", "Register", "Login"])
+
+if selection == "Home":
+    home.show()
+elif selection == "Register":
+    register.show()
+elif selection == "Login":
+    login.show()
+elif selection == "My Profile":
+    profile.show()
+elif selection == "Logout":
+    st.session_state["user_email"] = None
+    st.experimental_rerun()
