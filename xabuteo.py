ccbce3d81c667f2a0aeec93c53929e7f1530f407
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-22 17:02:42 +1000

    Update xabuteo.py

--- xabuteo.py
+++ xabuteo.py
@@ -23,7 +23,7 @@ authenticator = stauth.Authenticate(
 )
 
 # Perform login
-name, auth_status, username = authenticator.login("Login", location="main")
+name, auth_status, username = authenticator.login(form_name="Login", location="main")
 
 # Set session state on successful login
 if auth_status:
